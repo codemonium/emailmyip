bb9db95f0bf673af7ac30193f25c9c69dae21316
Igor Artemenko <igor@codemonium.com>
2021-08-29 15:45:58 -0700

    Fix path to .env for pyinstaller

--- emailmyip.py
+++ emailmyip.py
@@ -1,12 +1,20 @@
 import dotenv
 import os
 import requests
+import sys
 from postmarker.core import PostmarkClient
+
+
+def resource_path(relative_path):
+  # https://stackoverflow.com/a/44352931/10183593
+  base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+  return os.path.join(base_path, relative_path)
+
 
 response = requests.get("https://checkip.amazonaws.com")
 ip_address = response.text.strip()
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(dotenv_path=resource_path(".env"))
 
 postmark_api_key = os.environ['POSTMARK_API_KEY']
 from_email_address = os.environ['FROM_EMAIL_ADDRESS']
