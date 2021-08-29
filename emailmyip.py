4d7ea16bedc07a3d66d603df79d22c89033788ed
Igor Artemenko <igor@codemonium.com>
2021-08-29 12:48:36 -0700

    Send email using Postmark

--- emailmyip.py
+++ emailmyip.py
@@ -1,6 +1,17 @@
+import os
+
+from postmarker.core import PostmarkClient
 from urllib import request
 
 req = request.Request("https://checkip.amazonaws.com", data=None)
 res = request.urlopen(req, timeout=5)
 
-print(res.read().decode().strip())
+ip_address = res.read().decode().strip()
+
+postmark = PostmarkClient(server_token=os.environ['POSTMARK_API_KEY'])
+postmark.emails.send(
+  From=os.environ['FROM'],
+  To=os.environ['TO'],
+  Subject='Current IP Address',
+  HtmlBody=ip_address
+)
