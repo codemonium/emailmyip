62506535e429633f7e8f551b113685767746206c
Igor Artemenko <igor@codemonium.com>
2021-08-29 13:01:04 -0700

    Load environment variables using dotenv

--- emailmyip.py
+++ emailmyip.py
@@ -1,3 +1,4 @@
+import dotenv
 import os
 import requests
 from postmarker.core import PostmarkClient
@@ -5,10 +6,16 @@ from postmarker.core import PostmarkClient
 response = requests.get("https://checkip.amazonaws.com")
 ip_address = response.text.strip()
 
-postmark = PostmarkClient(server_token=os.environ['POSTMARK_API_KEY'])
+dotenv.load_dotenv()
+
+postmark_api_key = os.environ['POSTMARK_API_KEY']
+from_email_address = os.environ['FROM_EMAIL_ADDRESS']
+to_email_address = os.environ['TO_EMAIL_ADDRESS']
+
+postmark = PostmarkClient(server_token=postmark_api_key)
 postmark.emails.send(
-  From=os.environ['FROM'],
-  To=os.environ['TO'],
+  From=from_email_address,
+  To=to_email_address,
   Subject='Current IP Address',
   HtmlBody=ip_address
 )
